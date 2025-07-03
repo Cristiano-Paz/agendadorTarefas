f68079de03af895c51c8d8c54bbe2a7fe19a4565
Cristiano-Paz <cristianogspaz@gmail.com>
2025-07-03 15:49:21 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/java/agendadorTarefas/business/UsuarioDTO.java
@@ -0,0 +1,16 @@
+package com.java.agendadorTarefas.business;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private String email;
+    private String senha;
+}
