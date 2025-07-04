74a9661ac0d6addcd2dd80e0270f665fe0a4f8ab
Cristiano-Paz <cristianogspaz@gmail.com>
2025-07-04 16:30:05 -0300

    adicionados metodos Get por email e por periodo

--- src/main/java/com/java/agendadorTarefas/infrastructure/repository/TarefasRepository.java
+++ src/main/java/com/java/agendadorTarefas/infrastructure/repository/TarefasRepository.java
@@ -1,9 +1,17 @@
 package com.java.agendadorTarefas.infrastructure.repository;
 
 import com.java.agendadorTarefas.infrastructure.entity.TarefasEntity;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TarefasRepository extends MongoRepository<TarefasEntity, String> {
+
+    List<TarefasEntity> findByDataEventoBetween(LocalDateTime dataInicial, LocalDateTime dataFinal);
+
+    List<TarefasEntity> findByEmailUsuario(String eamil);
 }
