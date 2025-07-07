dbb1c57d4ce80e621c4a342f0986920e044f6aa0
Cristiano-Paz <cristianogspaz@gmail.com>
2025-07-07 11:53:09 -0300

    adicionados métodos de delete, patch de status e put de tarefa

---
+++ src/main/java/com/java/agendadorTarefas/business/mapper/TarefaUpdateConverter.java
@@ -0,0 +1,13 @@
+package com.java.agendadorTarefas.business.mapper;
+
+import com.java.agendadorTarefas.business.dto.TarefasDTO;
+import com.java.agendadorTarefas.infrastructure.entity.TarefasEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface TarefaUpdateConverter {
+
+    void updateTarefas(TarefasDTO dto, @MappingTarget TarefasEntity entity);
+}
